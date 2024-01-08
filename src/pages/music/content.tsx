df2c163754ea13de24e704da5623af5dd8d373f9
xtxd-yym <79433117+xtxd-yym@users.noreply.github.com>
2024-01-08 22:35:41 +0800

    feat: no.4 新增资源管理网站 v0.5 (#10)

--- src/pages/music/content.tsx
+++ src/pages/music/content.tsx
@@ -1,22 +1,88 @@
 import './style/index.less';
-import { memo, useCallback, useContext, useEffect, useState } from 'react';
-import { List } from 'antd';
+import { memo, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { Col, Row } from 'antd';
 import Notice from '@/components/source-notice';
 import { SourceMusicPrefix } from '@/constant/styles/index';
-import { getHomeNewModuleData } from '@/api/content';
+import { getSourceMusicMainData } from '@/api/content';
 import SourceTable from '@/components/source-table';
+import { AnyObject } from 'antd/es/_util/type';
 
 interface NewModuleDataType {
   title: string;
   listData: string[];
 }
 
+interface TableDataType {
+  id: string;
+  title: string;
+  url: string;
+}
+
 const Music = memo(() => {
   const prefix = SourceMusicPrefix;
-  
+  const [tableData, setTableData] = useState<TableDataType[][]>([[]]);
+
+  useEffect(() => {
+    const params = {};
+    getSourceMusicMainData(params)
+      .then((res) => {
+        if (res?.status === 200) {
+          hadnleTableData(res?.data || {});
+        } else {
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  //处理音乐Card接口数据
+  const hadnleTableData = (data: AnyObject = {}) => {
+    const { row = 5, col = 4, count = 20, imgData = [] } = data;
+    const imgTableData: TableDataType[][] = new Array(row).fill(0).map(() => new Array(col).fill(0));
+    let index = 0;
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < col; j++) {
+        if (index >= imgData.length) {
+          break;
+        }
+
+        imgTableData[i][j] = imgData[index];
+        index++;
+      }
+    }
+
+    setTableData(imgTableData);
+  };
+
+  //音乐Card渲染方法
+  const musicCardRender = useCallback((data: TableDataType) => {
+    const { id = '', title = '', url = '' } = data || {};
+    return (
+      <div className={`${prefix}-container-card`}>
+        <div className={`${prefix}-container-card-img`}>
+          <img src={`http://localhost:3000${url}`}></img>
+        </div>
+        <div className={`${prefix}-container-card-title`}>{title}</div>
+        <div className={`${prefix}-container-card-bottom`}></div>
+      </div>
+    );
+  }, []);
+
   return (
     <div className={prefix}>
-      同人音声
+      <div className={`${prefix}-tab`}></div>
+      <div className={`${prefix}-container`}>
+        {tableData.map((item, index) => (
+          <Row key={index}>
+            {item.map((data, dataIndex) => (
+              <Col key={dataIndex} span={Math.floor(24 / (item?.length || 1))}>
+                {musicCardRender(data)}
+              </Col>
+            ))}
+          </Row>
+        ))}
+      </div>
     </div>
   );
 });
